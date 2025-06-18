b4bcd6d33f77904e2725c07b47cdb658ef040067
edumoro412 <edumoro2003@gmail.com>
2025-06-19 00:09:46 +0200

    fix(app): replace RouterOutlet with MainLayout in app component imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainLayout],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'StockUp';
