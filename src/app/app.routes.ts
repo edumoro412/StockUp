427ea30c94b4c3f1aadbc32de2be224b972307ec
edumoro412 <edumoro2003@gmail.com>
2025-06-19 00:09:42 +0200

    feat(routes): define initial routing structure with MainLayout and Home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Home } from './Pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+    ],
+  },
+];
