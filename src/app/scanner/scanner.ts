9e4736967093605f233aa3cf4b3e811d2149f2d5
edumoro412 <edumoro2003@gmail.com>
2025-06-19 19:46:25 +0200

    feat(scanner): add scanner component with HTML, SCSS, and unit tests

---
+++ src/app/scanner/scanner.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Html5Qrcode } from 'html5-qrcode';
+
+@Component({
+  selector: 'app-scanner',
+  standalone: true,
+  imports: [],
+  templateUrl: './scanner.html',
+  styleUrl: './scanner.scss',
+})
+export class Scanner implements OnInit, OnDestroy {
+  private html5QrCode!: Html5Qrcode;
+
+  ngOnInit(): void {
+    this.startScanner();
+  }
+
+  ngOnDestroy(): void {
+    this.html5QrCode?.stop().catch((err) => console.log(err));
+  }
+
+  startScanner(): void {
+    const config = {
+      fps: 10,
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+    };
+
+    this.html5QrCode = new Html5Qrcode('scanner');
+    this.html5QrCode
+      .start(
+        { facingMode: 'environment' },
+        config,
+        (decodeText) => {
+          console.log('Código escaneado:', decodeText);
+          this.fetchProduct(decodeText);
+          this.html5QrCode.stop();
+        },
+        (errorMessage) => {
+          console.warn('Error al escanear: ', errorMessage);
+        }
+      )
+      .catch((err) => console.log('Error al iniciar escáner:', err));
+  }
+
+  fetchProduct(code: string): void {
+    const isBarcode = /^\d{8,14}$/.test(code);
+
+    if (!isBarcode) {
+      console.warn('No es un código de barras válido');
+      return;
+    }
+
+    fetch(`https://world.openfoodfacts.org/api/v0/product/${code}.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 1) {
+          console.log('Producto:', data.product);
+        } else {
+          console.warn('Producto no encontrado');
+        }
+      })
+      .catch((err) => {
+        console.error('Error al hacer la petición:', err);
+      });
+  }
+}
