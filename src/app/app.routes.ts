f038ef8809664a511ec2d3f691d286405e3869dd
edumoro412 <edumoro2003@gmail.com>
2025-06-19 19:45:20 +0200

    refactor(routes): remove MainLayout routing structure and simplify routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,4 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from './layouts/main-layout/main-layout';
 import { Home } from './Pages/home/home';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: MainLayout,
-    children: [
-      {
-        path: '',
-        component: Home,
-      },
-    ],
-  },
-];
+export const routes: Routes = [];
