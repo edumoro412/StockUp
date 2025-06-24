9666a369d8b0490f97cec08a0913695815ffea5f
edumoro412 <edumoro2003@gmail.com>
2025-06-24 17:54:51 +0200

    feat(routes): define routes for Home and Scanner components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './Pages/home/home';
+import { Scanner } from './scanner/scanner';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'scanner',
+    component: Scanner,
+  },
+];
