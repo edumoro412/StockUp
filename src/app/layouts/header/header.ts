6c69566a92d295a38c7a600a100d560b089ca273
edumoro412 <edumoro2003@gmail.com>
2025-06-24 17:55:00 +0200

    feat(header): add navigation to home and update scanner link

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
-
+  constructor(private router: Router) {}
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
