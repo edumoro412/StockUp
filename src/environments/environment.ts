639f63263dc01aa83ba67b8253ee0d0d17232d8c
edumoro412 <edumoro2003@gmail.com>
2025-07-04 14:32:47 +0200

    feat(environment): add environment configuration for Supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://zzksyqqtfwrjfigswogc.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6a3N5cXF0ZndyamZpZ3N3b2djIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MzA2NzIsImV4cCI6MjA2NzIwNjY3Mn0.OaWuF5lZIbyCdXJ-0rr0jilzj-1YNFjgN2wEecoBPSc',
+};
