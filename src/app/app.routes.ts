5ec0efd7d172bed87ba19972efe4a896caa26c0b
edumoro412 <edumoro2003@gmail.com>
2025-07-31 17:20:26 +0200

    feat(routes): add authGuard to home route for access control

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './Pages/home/home';
 import { Scanner } from './scanner/scanner';
 import { LogIn } from './Pages/log-in/log-in';
 import { Register } from './Pages/register/register';
+import { authGuard } from './auth-guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+    canActivate: [authGuard],
   },
   {
     path: 'scanner',
