f2c1d71198c92d5af6275f007b844de9224fe5a6
edumoro412 <edumoro2003@gmail.com>
2025-09-12 18:58:58 +0200

    feat(header): Added the new logo to the header, and implemented the RouterLinkActive

--- src/app/layouts/header/header.ts
+++ src/app/layouts/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
