0409dd044bafbff730f226899aeaeed719eb9e82
edumoro412 <edumoro2003@gmail.com>
2025-09-13 14:47:04 +0200

    fix(interfaces): Fixed the openfoodfacts interface to get just what i need

--- src/app/interfaces/open-food-facts.ts
+++ src/app/interfaces/open-food-facts.ts
@@ -1,30 +1,25 @@
 type Nutriments = {
-  energy_kcal: number;
+  energy: number;
   fat: number;
-  saturated_fat: number;
   carbohydrates: number;
-  sugars: number;
-  fiber: number;
   proteins: number;
-  salt: number;
-  sodium: number;
 };
 
-type Product = {
+type NutriScoreType = {
+  grade?: string;
+};
+type ProductType = {
   product_name: string;
   image_url: string;
-  categories: string;
-  brands: string;
   code: string;
   nutriments: Nutriments;
-  serving_size?: string;
-  nutrition_score_grade?: string;
-  nova_group?: number;
+  nutrition_grades: string | null;
+  quantity: string | null;
   additives_tags?: string[];
 };
 
 type OpenFoodFactsResponse = {
   status: number;
   code: string;
-  product: Product;
+  product: ProductType;
 };
