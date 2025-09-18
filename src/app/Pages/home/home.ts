badd3a76c62c0958775e4a08a2dfe03f486d6e4b
edumoro412 <edumoro2003@gmail.com>
2025-09-18 12:50:46 +0200

    feat(home): added the new function for the name and in the template too

--- src/app/Pages/home/home.ts
+++ src/app/Pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Header } from '../../layouts/header/header';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,15 @@ import { Header } from '../../layouts/header/header';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnInit {
+  name: string = 'Usuario';
+
+  constructor(private supabaseService: SupabaseService) {}
+  ngOnInit(): void {
+    this.supabaseService.getUserName().then((name) => {
+      if (name) {
+        this.name = name;
+      }
+    });
+  }
+}
