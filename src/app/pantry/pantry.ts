20660b7894ad6d16bdc3fd14be903a8d66efd2cf
edumoro412 <edumoro2003@gmail.com>
2025-09-22 13:28:52 +0200

    feat(pantry): implemented some new functions and components

--- src/app/pantry/pantry.ts
+++ src/app/pantry/pantry.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { SupabaseService } from '../services/supabase.service';
+import { ProductCard } from '../component/product-card/product-card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pantry',
-  imports: [],
+  imports: [ProductCard, CommonModule],
   templateUrl: './pantry.html',
-  styleUrl: './pantry.scss'
+  styleUrl: './pantry.scss',
 })
-export class Pantry {
-
+export class Pantry implements OnInit {
+  data: PantryProductType[] = [];
+  constructor(private supabase: SupabaseService) {}
+  async ngOnInit(): Promise<void> {
+    this.data = await this.supabase.getProductsOfCurrentUser();
+    console.log(this.data);
+  }
 }
