185588a88b756dcbb63e741049ff9e0a7272b968
edumoro412 <edumoro2003@gmail.com>
2025-09-22 12:39:24 +0200

    feat)pantry): created the pantry component

---
+++ src/app/pantry/pantry.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantry',
+  imports: [],
+  templateUrl: './pantry.html',
+  styleUrl: './pantry.scss'
+})
+export class Pantry {
+
+}
