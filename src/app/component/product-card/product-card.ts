92b8e2486474b8bd25a4e2284e4de3edb6eedf0a
edumoro412 <edumoro2003@gmail.com>
2025-09-23 14:24:12 +0200

    feat(product-card): new functions for the new two buttons

--- src/app/component/product-card/product-card.ts
+++ src/app/component/product-card/product-card.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-product-card',
@@ -13,9 +14,28 @@ export class ProductCard {
   @Input() quantity!: number;
   @Input() id!: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private supabase: SupabaseService) {}
 
   goToDetails() {
     this.router.navigate(['/product', this.id]);
+  }
+
+  async increaseQuantity(product: ProductType) {
+    if (product.quantity) {
+      product.quantity += 1;
+      const userId = await this.supabase.getUserId();
+      if (userId) {
+        await this.supabase.addToPantry(userId, this.id, 1);
+      }
+    }
+    return;
+  }
+
+  async decreaseButton(product: ProductType) {
+    if (product.quantity && Number(product.quantity) > 1) {
+      await this.supabase.decreaseQuantity(product.code);
+    } else if (product.quantity ?? Number(product.quantity) == 1) {
+      await this.supabase.deletePantryItem(product.code);
+    }
   }
 }
