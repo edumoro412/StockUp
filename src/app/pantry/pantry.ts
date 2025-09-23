0b5cfea04ae1b8355f7891fc1140fb216fe19c48
edumoro412 <edumoro2003@gmail.com>
2025-09-23 15:16:46 +0200

    feat(pantry): pantry functionality works and at first its also finished

--- src/app/pantry/pantry.ts
+++ src/app/pantry/pantry.ts
@@ -12,9 +12,35 @@ import { CommonModule } from '@angular/common';
 })
 export class Pantry implements OnInit {
   data: PantryProductType[] = [];
+
   constructor(private supabase: SupabaseService) {}
   async ngOnInit(): Promise<void> {
     this.data = await this.supabase.getProductsOfCurrentUser();
     console.log(this.data);
+  }
+
+  async increaseQuantity(product: PantryProductType) {
+    product.isLoading = true;
+    if (product.quantity) {
+      product.quantity += 1;
+      const userId = await this.supabase.getUserId();
+      if (userId) {
+        await this.supabase.addToPantry(userId, product.product_id, 1);
+        product.isLoading = false;
+      }
+    }
+    return;
+  }
+
+  async decreaseButton(product: PantryProductType) {
+    product.isLoading = true;
+    if (product.quantity && Number(product.quantity) > 1) {
+      await this.supabase.decreaseQuantity(product.product_id);
+      product.quantity -= 1;
+    } else if (product.quantity ?? Number(product.quantity) == 1) {
+      await this.supabase.deletePantryItem(product.product_id);
+      this.data = this.data.filter((p) => p.product_id !== product.product_id);
+    }
+    product.isLoading = false;
   }
 }
