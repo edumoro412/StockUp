c5be914b144bdb62926d5308f48eb79cb8199e52
edumoro412 <edumoro2003@gmail.com>
2025-10-16 12:54:41 +0200

    feat(pantry): added a new input to filter the products

--- src/app/Pages/pantry/pantry.ts
+++ src/app/Pages/pantry/pantry.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { SupabaseService } from '../../services/supabase.service';
 import { ProductCard } from '../../component/product-card/product-card';
-import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 @Component({
   selector: 'app-pantry',
-  imports: [ProductCard, CommonModule, RouterLink],
+  imports: [ProductCard, ReactiveFormsModule, RouterLink],
   templateUrl: './pantry.html',
   styleUrl: './pantry.scss',
 })
 export class Pantry implements OnInit {
   data: PantryProductType[] = [];
+  filteredData: PantryProductType[] = [];
+  searchControl = new FormControl('');
   loading: boolean = false;
 
   constructor(private supabase: SupabaseService) {}
@@ -20,6 +22,17 @@ export class Pantry implements OnInit {
     try {
       this.loading = true;
       this.data = await this.supabase.getProductsOfCurrentUser();
+      this.filteredData = [...this.data];
+
+      this.searchControl.valueChanges
+        .pipe(debounceTime(300), distinctUntilChanged())
+        .subscribe((query) => {
+          this.filteredData = this.data.filter((p) =>
+            p.product_name
+              .toLowerCase()
+              .includes(query?.toLocaleLowerCase() || '')
+          );
+        });
     } catch (err) {
       console.log('Hubo un error al cargar la despensa', err);
     } finally {
