7ba66d42fd09b6d5ebdbd9f17a42fb11ff012c2e
edumoro412 <edumoro2003@gmail.com>
2025-10-16 13:36:55 +0200

    feat(favorites-card): now the new filter functionality works here too, and a lot of code have been removed

--- src/app/component/favorites-card/favorites-card.ts
+++ src/app/component/favorites-card/favorites-card.ts
@@ -1,31 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { SupabaseService } from '../../services/supabase.service';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-favorites-card',
   imports: [],
   templateUrl: './favorites-card.html',
   styleUrl: './favorites-card.scss',
 })
-export class FavoritesCard implements OnInit {
-  data: FavoritesProductType[] | null = null;
-  loading: boolean = false;
-
-  constructor(private supabase: SupabaseService, private route: Router) {}
-  async ngOnInit(): Promise<void> {
-    try {
-      this.loading = true;
-      const favorites = await this.supabase.getFavorites();
-      if (favorites.success) {
-        this.data = favorites.data;
-      }
-    } catch (error) {
-    } finally {
-      this.loading = false;
-    }
-  }
+export class FavoritesCard {
+  @Input() data!: FavoritesProductType[] | null;
+  @Output() navigate = new EventEmitter();
 
   goDetails(id: string) {
-    this.route.navigate(['/product', id]);
+    this.navigate.emit(id);
   }
 }
